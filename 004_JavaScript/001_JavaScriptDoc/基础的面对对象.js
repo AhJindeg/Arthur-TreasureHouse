203fe8bde0db99390c36da673bb49161ca23c799
Arhtur <AhJindeg@163.com>
2022-06-08 16:54:26 +0800

    :sparkles: 基础面对对象 增加监听和计算属性 get&set

--- 004_JavaScript/001_JavaScriptDoc/基础的面对对象.js
+++ 004_JavaScript/001_JavaScriptDoc/基础的面对对象.js
@@ -124,3 +124,101 @@ for (var name in objectC) {
   console.log(name) //salary 不可遍历, 不可见
 }
 objectC.id = 2222
+
+/**
+ * getter: 计算属性
+ * 计算属性get  跟 value/writable 配置项是互斥的
+ * get: 获取,  当读取一个属性时, 会自动触发 get属性的函数, 把此函数的返回值 作为属性的值
+ * 语法糖写法, 固定格式,  计算属性
+ * 使用时: 对象.area 与函数方法少写()
+ */
+// 矩形:
+let Wha = {
+  width: 100,
+  height: 40,
+  get sum() {
+    return (this.width + this.height) * 2
+  },
+}
+Object.defineProperty(Wha, 'area', {
+  get: function () {
+    console.log('有人要读取面积!')
+    return this.width * this.height
+  },
+})
+console.log(Wha.area)
+console.log(Wha.sum)
+
+/**
+ * 赋值监听 setter
+ * set: 用来监听属性的赋值
+ * 修改属性的 set, 在此方法中接受赋值, 然后对值进行检测, 如果正确才赋值-- 此处不能给当前属性赋值, 会无限循环 -- 此处必须额外声明一个变量, 来存储值
+ * 读取时, 再通过 get 方法, 来返回那个存值的变量
+ */
+let meng = {
+  name: '大猛',
+  age: 18,
+}
+// 生成一个额外的变量, 来保存age的值, 防止下方的无限循环
+//__没有特殊作用, 习惯上把这种专门负责保存 set 方法设定的属性值的额外属性, 加__前缀, 以示特殊!
+let __age
+// 要求：大猛年龄范围是1-100 超过就报错
+Object.defineProperty(meng, 'age', {
+  // 赋值,设置值: set
+  set: function (value) {
+    if (value >= 1 && value <= 100) {
+      __age = value //赋值, 会触发 age 的set方法
+    } else {
+      throw Error('年龄范围错误: ' + value)
+    }
+  },
+  // 读取时 返回 __age 的值, 这个变量的值是 set 设置的
+  get: function () {
+    return __age
+  },
+})
+meng.age = 50 //应该报错!
+console.log(meng.age) //触发get
+// 匿名函数自调用, 就可以形成一个 闭包作用域
+// set 和 get 方法都存储在 meng, meng 在window里
+;(function () {
+  let __salary
+  Object.defineProperty(meng, 'salary', {
+    get: function () {
+      return __salary
+    },
+    set: function (value) {
+      if (value >= 1000 && value <= 100000) {
+        __salary = value
+      } else {
+        throw Error('薪资范围错误' + value)
+      }
+    },
+  })
+})()
+meng.salary = 11250 //正常
+console.log(meng.salary)
+
+/**
+ * setter 语法糖
+ * __age: 应该是一个不可遍历的属性, 藏起来, 是搭配age使用的附庸产物
+ * 默认 使用此方式新增的属性, 所有配置项都是false, 即 不可写, 不可遍历, 不可重新配置.   此处的__age 应该是可写的才能存储值
+ */
+let daMeng = {
+  name: '大猛',
+  set age(value) {
+    if (value >= 1 && value <= 100) {
+      // var a = {}; a.__age = 3;
+      // 就会在当前对象中自动创建__age属性
+      this.__age = value
+    } else {
+      throw Error('年龄范围错误!')
+    }
+  },
+  get age() {
+    return this.__age
+  },
+}
+Object.defineProperty(daMeng, '__age', { writable: true })
+daMeng.age = 50
+console.log(daMeng)
